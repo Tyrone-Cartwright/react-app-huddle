521638c1b7089323838fcc79e90b14f8ad49e679
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-05-02 16:51:49 -0400

    modules

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,11 +1,33 @@
 import logo from "./img/logo.svg"
+import mockup from "./img/illustration-mockups.svg"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+
+      <header>
+        <img src={logo} alt="huddle logo"/>
+      </header>
+
+      <main>
+        <img src={mockup} alt="mockup illustration"/>
+        <div className="content">
+          <h2>Build the community your fans will love</h2>
+          <p>Huddle re-imagines the way we build communities. You have a voice, but so
+            does your audience. Create connections with your users as you engage in genuine discussion.
+          </p>
+          <button>Register</button>
+        </div>
+      </main>
+
+      <footer>
+      <i class="fab fa-facebook-f"></i>
+      <i class="fab fa-twitter"></i>
+      <i class="fab fa-instagram"></i>
+      </footer>
      
-    </div>
+    </>
   );
 }
 
