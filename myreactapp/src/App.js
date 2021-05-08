444aa443addf0938cf00cf4e3bdb20c2589d651c
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-05-08 15:54:15 -0400

    added component folder and Header folder

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,32 +1,29 @@
-import logo from "./img/logo.svg"
-import mockup from "./img/illustration-mockups.svg"
+import mockup from './img/illustration-mockups.svg';
 import './App.css';
+import Header from './Component/Header';
 
 function App() {
   return (
     <>
-
-      <header>
-        <img src={logo} alt="huddle logo"/>
-      </header>
-
+      <header />
       <main>
-        <img src={mockup} alt="mockup illustration"/>
-        <div className="content">
+        <img src={mockup} alt='mockup illustration' />
+        <div className='content'>
           <h2>Build the community your fans will love</h2>
-          <p>Huddle re-imagines the way we build communities. You have a voice, but so
-            does your audience. Create connections with your users as you engage in genuine discussion.
+          <p>
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion.
           </p>
           <button>Register</button>
         </div>
       </main>
 
       <footer>
-      <i class="fab fa-facebook-f"></i>
-      <i class="fab fa-twitter"></i>
-      <i class="fab fa-instagram"></i>
+        <i class='fab fa-facebook-f'></i>
+        <i class='fab fa-twitter'></i>
+        <i class='fab fa-instagram'></i>
       </footer>
-     
     </>
   );
 }
---
+++ myreactapp/src/Component/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../img/logo.svg';
+
+const Header = () => {
+  return <img src={logo} alt='huddle logo' />;
+};
+
+export default Header;
